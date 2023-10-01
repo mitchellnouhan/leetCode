7a13cfc30018a451035284127c5198e68a38ddee
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2023-09-30 17:08:44 -0700

    [Add] my attempt at the Two Sum II problem

---
+++ twoPointers/twoSumII.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        #the big idea: assign start and end indices, figure out which index is 
+        #the problem every iteration,then adjust problem index
+        lowInd = 0
+        highInd = len(numbers) - 1
+        while (numbers[lowInd] + numbers[highInd] != target):
+            if target - numbers[lowInd] > numbers[highInd]:
+                #low index is the problem
+                lowInd += 1
+            elif target - numbers[highInd] < numbers[lowInd]:
+                #high index is the problem
+                highInd -= 1
+        return [lowInd + 1, highInd + 1]
