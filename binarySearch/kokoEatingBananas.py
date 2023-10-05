d0ceed4b83c0fc91b69a50c73a035dc6e95a2e20
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2023-10-05 12:26:47 -0700

    My second iteration at Koko Eating Bananas challenge

--- binarySearch/kokoEatingBananas.py
+++ binarySearch/kokoEatingBananas.py
@@ -1,5 +1,3 @@
-#my first go at the Koko Eating Bananas problem
-
 class Solution(object):
     def timeItTakesToEat(self, piles, speed):
         time = 0
@@ -8,34 +6,14 @@ class Solution(object):
         return int(time)
 
     def minEatingSpeed(self, piles, h):
-        """
-        :type piles: List[int]
-        :type h: int
-        :rtype: int
-        """
-        #course tune w/ binary pick
-        piles.sort()
-        fineTune = 0
-        pilesLen = len(piles)
         l = 1
-        r = piles[pilesLen - 1]
-        while fineTune == 0 and l <= r:
+        r = max(piles)
+        while l <= r:
             m = l + (r - l) / 2
-            simulationResult = h - self.timeItTakesToEat(piles, m)
-            #we ate too fast
-            if simulationResult > 0:
+            #we ate too fast 
+            if h - self.timeItTakesToEat(piles, m) >= 0:
                 r = m - 1
             #we ate too slow
-            elif simulationResult < 0:
-                l = m + 1
             else:
-                fineTune = m
-        
-        #fine tune
-        if l > r:
-            return l
-        
-        while fineTune - 1 and h - self.timeItTakesToEat(piles, fineTune - 1) == 0:
-            fineTune -= 1
-        
-        return fineTune
+                l = m + 1
+        return l
