5e8e399fe0beb24ec6aefcf1d3c9144129f2ffd6
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2023-10-10 18:16:18 -0700

    [modify] logic to longest repeating character replacement in big O of 26n time complexity

--- slidingWindow/longestRepeatingCharacterReplacement.py
+++ slidingWindow/longestRepeatingCharacterReplacement.py
@@ -2,16 +2,12 @@ class Solution(object):
     def characterReplacement(self, s, k):
         numberOfLetters = 26
         charArr = [0] * numberOfLetters
-        l, r = 0, 0
+        l = 0
         maxLen = 0
-        sLen = len(s)
-        while r <= sLen:
-            if (r - l) - max(charArr) > k:
+        for r in range(len(s)):
+            charArr[ord(s[r]) - ord("A")] += 1
+            while (r - l + 1) - max(charArr) > k:
                 charArr[ord(s[l]) - ord("A")] -= 1
                 l += 1
-            else:
-                maxLen = max(maxLen, r - l)
-                if r < sLen:
-                    charArr[ord(s[r]) - ord("A")] += 1
-                r += 1         
+            maxLen = max(maxLen, (r - l + 1))
         return maxLen
