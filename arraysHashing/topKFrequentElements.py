1e5fa033be301c87492f2c7878e467b9aec07dc1
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.lan>
2023-12-04 16:35:52 -0800

    [modify] Top K Frequent Elements

--- arraysHashing/topKFrequentElements.py
+++ arraysHashing/topKFrequentElements.py
@@ -1,21 +1,7 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        #use a frequency based hash map
-        #create an hashmap:
-        freqHash = defaultdict(lambda: 0)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(lambda: 0)
         for num in nums:
-            #(key,val) = (unique num in nums, frequency it appears in nums)
-            freqHash[num] += 1
-        #sort hash map based on its freq values
-        freqHash = sorted(freqHash.items(),key=lambda x: x[1],reverse=True)
-        kFreqElements = []
-        for i in range(k):
-            item = freqHash[i][0]
-            kFreqElements.append(item)
-        #return the first/last k indexes of sorted hashmap
-        return kFreqElements
+            freq[num] += 1
+        freq = dict(sorted(freq.items(), key = lambda x: x[1], reverse=True))
+        return list(freq.keys())[0:k]
