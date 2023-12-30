f6892c2914b18e939840fc99cf6b6152003b2713
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2023-12-30 10:49:35 -0800

    [modify] my home cooked solution to Valid Palindrome

--- twoPointers/validPalindrome.py
+++ twoPointers/validPalindrome.py
@@ -1,34 +1,15 @@
-#my crack at valid palindrome
-class Solution(object):
-    def lowerCaseIt(self, char):
-        if char >= "A" and char <= "Z":
-            return chr(ord(char) + 32)
-        return char
-
-    def isAlphaNum(self, char):
-        if char >= "A" and char <= "Z":
-            return True
-        elif char >= "a" and char <= "z":
-            return True
-        elif char >= "0" and char <= "9":
-            return True
-        return False
-    
-    def isPalindrome(self, s):
-        left = 0
-        right = len(s) - 1
-        while left <= right:
-            if self.isAlphaNum(s[left]) and self.isAlphaNum(s[right]):
-                if self.lowerCaseIt(s[left]) != self.lowerCaseIt(s[right]):
-                    return False
-                left += 1
-                right -= 1
-            #if not, adjust left and right indexes as needed for next cycle
-            elif not self.isAlphaNum(s[left]) and self.isAlphaNum(s[right]):
-                left += 1
-            elif self.isAlphaNum(s[left]) and not self.isAlphaNum(s[right]):
-                right -= 1
-            else:
-                left += 1
-                right -= 1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l <= r:
+            while l <= r and not s[l].isalnum():
+                l += 1
+            while l <= r and not s[r].isalnum():
+                r -= 1
+            if l > r:
+                return True
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
