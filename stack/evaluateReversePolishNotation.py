00cf3c93ba3cc43928ec000c1f2bf7b28525e5bd
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2024-01-13 14:05:13 -0800

    [modify] solution to Evaluate Reverse Polish Notation

--- stack/evaluateReversePolishNotation.py
+++ stack/evaluateReversePolishNotation.py
@@ -1,39 +1,19 @@
-class Solution(object):
-    def Operation(self, operand1, operand2, operator):
-        if operator == '+':
-            return operand1 + operand2
-        elif operator == '-':
-            return operand1 - operand2
-        elif operator == '*':
-            return operand1 * operand2
-        else:
-            operand1 = float(operand1)
-            operand2 = float(operand2)
-            if operand1 / operand2 < 0:
-                return math.ceil(operand1 / operand2)
-            return math.floor(operand1 / operand2)
-    def isOperator(self, token):
-        if token == '+' or token == '-' or token == '*' or token == '/':
-            return True
-        return False
-    def evalRPN(self, tokens):
-        """
-        :type tokens: List[str]
-        :rtype: int
-        """
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        #iterate thru tokens
         for token in tokens:
-            #if token is operator
-            if self.isOperator(token):
-                #pop 2 operands off
-                operand2 = int(stack.pop(-1))
-                operand1 = int(stack.pop(-1))
-                #evalute expression, then push result back onto the stack
-                new = self.Operation(operand1,operand2,token) 
-                stack.append(new)
+            if token == "*" or token == "/" or token == "+" or token == "-":
+                operandc, operandb, operanda = 0, stack.pop(), stack.pop()
+                if token == "*":
+                    operandc = operanda * operandb
+                elif token == "/":
+                    operandc = operanda // operandb
+                    operandc = (operandc + 1) if ((operandc < 0) and (operanda % operandb) != 0) else operandc
+                elif token == "+":
+                    operandc = operanda + operandb
+                else:
+                    operandc = operanda - operandb
+                stack.append(operandc) 
             else:
-                #push operands onto the stack
-                stack.append(token)
-        #if done properly, after iterating thru all tokens, a single value  should remain on the stack
-        return int(stack[0])
+                stack.append(int(token))
+        return stack.pop()  
