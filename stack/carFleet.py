463267385aa6d15c2d463ffd6241f35c1d64c658
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2024-01-17 15:58:30 -0800

    [modify] solution to Car Fleet

--- stack/carFleet.py
+++ stack/carFleet.py
@@ -1,16 +1,11 @@
-#Monotonic decreasing stack where elements being stacked are calculated times for group to reach target
-class Solution(object):
-    def carFleet(self, target, position, speed):
-
-        groups = []
-        timeToTarget = [0] * len(position)
-        for i in range(len(position)):
-            timeToTarget[i] = float((target - position[i])) / speed[i]
-
-        posSortedTimeToTarget = sorted(zip(position,timeToTarget), key = lambda x: x[0])
-        for carTime in posSortedTimeToTarget:
-            while len(groups) and groups[-1] <= carTime[1]:
-                groups.pop(-1)
-            groups.append(carTime[1])
-
-        return len(groups)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        for i in range(len(speed)):
+            speed[i] = (target - position[i]) / (speed[i])
+        times = [time for _, time in sorted(zip(position, speed))]
+        for time in times:
+            while len(stack) and time >= stack[-1]:
+                stack.pop()
+            stack.append(time)
+        return len(stack)
