0a5f6977f80381b97ec8cc06d8894e3c2ede383b
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.lan>
2024-01-22 16:25:12 -0800

    [modify] solution to Best Time to Buy and Sell Stock

--- slidingWindow/bestTimeToBuyAndSellStock.py
+++ slidingWindow/bestTimeToBuyAndSellStock.py
@@ -1,12 +1,11 @@
 class Solution(object):
-    def maxProfit(self, prices):
-        maxProfit = 0
-        priceLen = len(prices)
-        buy, sell = 0, 1
-        while sell < priceLen:
-            curProfit = prices[sell] - prices[buy]
-            maxProfit = max(maxProfit, curProfit)
-            if curProfit <= 0:
-                buy = sell
-            sell += 1
-        return maxProfit
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP, b, s = 0, 0, 0
+        while s < len(prices):
+            if prices[b] >= prices[s]:
+                b = s
+            else:
+                maxP = max(maxP, prices[s] - prices[b])
+            s += 1
+        return maxP
