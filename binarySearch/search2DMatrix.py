24e003c67d5c54394850b908d1f69e36651ab4aa
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.lan>
2024-01-22 17:41:54 -0800

    [modify] solution for Search 2D Matrix

--- binarySearch/search2DMatrix.py
+++ binarySearch/search2DMatrix.py
@@ -1,42 +1,22 @@
-class Solution(object):
-    def binarySearchRow(self, row, target):
-        l = 0
-        h = len(row) - 1
-        while l <= h:
-            m = l + (h - l) / 2
-            if target < row[m]:
-                h = m - 1
-            elif target > row[m]:
-                l = m + 1
-            else:
-                return True
-        return False 
-
-    def binarySearchFirstColumn(self, matrix, target):
-        #base case
-        row = -1
-        if target < matrix[0][0]:
-            return row
-        rows = len(matrix)
-        l = 0
-        h = rows - 1
-        while l <= h:
-            m = l + (h - l) / 2
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        l, r = 0, len(matrix) - 1
+        while l <= r:
+            m = l + (r - l // 2)
             if target < matrix[m][0]:
-                h = m - 1
+                r = m - 1
             elif target > matrix[m][0]:
                 l = m + 1
             else:
-                return -2
-        return h
-
-    def searchMatrix(self, matrix, target):
-        #perform a modified version of binary search on the 1st or 0th column to determine which row to look into
-        row = self.binarySearchFirstColumn(matrix, target)
-        #handle special function return codes
-        if row == -1:
-            return False    
-        elif row == -2:
-            return True
-        #then perform a binary search on the row the previous binary search lead you to look into
-        return self.binarySearchRow(matrix[row], target)
+                return True
+        row = max(min(l, r), 0)
+        l, r = 0, len(matrix[row]) - 1
+        while l <= r:
+            m = l + (r - l // 2)
+            if target < matrix[row][m]:
+                r = m - 1
+            elif target > matrix[row][m]:
+                l = m + 1
+            else:
+                return True
+        return False
