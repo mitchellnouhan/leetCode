ffc9fcc0cfcaa5edd289bf63771c74d7ca16277a
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.lan>
2024-01-23 13:51:22 -0800

    [modify] solution to Koko Eating Bananas

--- binarySearch/kokoEatingBananas.py
+++ binarySearch/kokoEatingBananas.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def timeItTakesToEat(self, piles, speed):
-        time = 0
-        for pile in piles:
-            time += ceil(float(pile)/speed)
-        return int(time)
-
-    def minEatingSpeed(self, piles, h):
-        l = 1
-        r = max(piles)
-        while l <= r:
-            m = l + (r - l) / 2
-            #we ate too fast 
-            if h - self.timeItTakesToEat(piles, m) >= 0:
-                r = m - 1
-            #we ate too slow
-            else:
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        while l < r:
+            m = l + (r - l) // 2
+            time = 0
+            for pile in piles:
+                time += math.ceil(pile / m)
+            if time > h:
                 l = m + 1
-        return l
+            else:
+                r = m 
+        return r  
