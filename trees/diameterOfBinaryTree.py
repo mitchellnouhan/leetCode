60f8ba592b081c332d49ee0361ca761045919ecf
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2024-07-21 18:57:53 -0700

    [modify] diameter of binary tree

--- trees/diameterOfBinaryTree.py
+++ trees/diameterOfBinaryTree.py
@@ -1,26 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxDepth(self, root):
-        if root is None:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if root is None or (root.left is None and root.right is None):
-            return 0
-        
-        leftMaxDepth = self.maxDepth(root.left)
-        rightMaxDepth = self.maxDepth(root.right)
-        rootDiameter = leftMaxDepth + rightMaxDepth
-        leftDiameter = self.diameterOfBinaryTree(root.left)
-        rightDiameter = self.diameterOfBinaryTree(root.right)
-        return max(rootDiameter, leftDiameter, rightDiameter)
+class Solution:
+    def __init__(self):
+        self.res = 0
+
+    def dfs(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        leftLen, rightLen = self.dfs(root.left), self.dfs(root.right)    
+        self.res = max(self.res, leftLen + rightLen)
+        return 1 + max(leftLen, rightLen)
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.dfs(root)
+        return self.res
