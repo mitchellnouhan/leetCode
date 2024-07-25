e2a8990ab1d834f0bb98a024483873e402043e19
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2024-07-25 02:11:19 -0700

    [modify] subtree of another tree to accurately handle edge case where root and subRoot are both null

--- trees/subtreeOfAnotherTree.py
+++ trees/subtreeOfAnotherTree.py
@@ -10,5 +10,5 @@ class Solution:
         return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root: return False
-        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)  
+        if not root: return (not root and not subRoot)
+        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
