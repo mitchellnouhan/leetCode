ad0635daadd0e651a63c9fa5f373e8658bff157a
Mitchell Nouhan <mitchellnouhan@Mitchells-Air.attlocal.net>
2024-07-25 02:07:16 -0700

    [modify] subtree of another tree

--- trees/subtreeOfAnotherTree.py
+++ trees/subtreeOfAnotherTree.py
@@ -1,33 +1,14 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):    
-    def isSameTree(self, root, subRoot):
-        """
-        :type root: TreeNode
-        :type subRoot: TreeNode
-        :rtype: bool
-        """
-        #needs to be same leftSub tree, and same rightSub tree
-        if (root and not subRoot) or (not root and subRoot):
-            return False
-        elif not root and not subRoot:
-            return True
-        elif root.val != subRoot.val:
-            return False
-        
-        return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
-        
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: TreeNode
-        :type subRoot: TreeNode
-        :rtype: bool
-        """
-        if root is None:
-            return False
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p or not q: return (not p and not q)
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) or self.isSameTree(root, subRoot)   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root: return False
+        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)  
